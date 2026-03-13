f3f6c87a6f8f7ea0910124e2a45d638b973891c4
vaishnavichorat <ivaishnavichorat@gmail.com>
2026-03-13 23:41:52 +0530

    commit changes

---
+++ src/hooks/useCurrencyinfo.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+      .then((res) => res.json())
+      .then((res) => setData(res.rates));
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfo;
+
